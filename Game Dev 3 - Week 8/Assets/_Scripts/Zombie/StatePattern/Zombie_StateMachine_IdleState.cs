15ab9e1f8e020ac9cd60009052383b23b0e438e5
ConorBuffey <c.buffey3244@student.leedsbeckett.ac.uk>
2025-01-05 21:27:35 +0000

    done

---
+++ Game Dev 3 - Week 8/Assets/_Scripts/Zombie/StatePattern/Zombie_StateMachine_IdleState.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+namespace GameDevWithMarco.StatePattern
+{
+    public class Zombie_StateMachine_IdleState : Zombie_StateMachine_BaseState
+    {
+        public override void EnterState(Zombie_StateMachine_Manager stateMachineManager)
+        {
+            stateMachineManager.animScript.PlayIdleAnimation();     //Plays the idle animation
+        }
+
+        public override void UpdateState(Zombie_StateMachine_Manager stateMachineManager)
+        {
+            if (stateMachineManager.zombieAiScript.isChasing == true)
+            {
+                stateMachineManager.SwitchState(stateMachineManager.movingState);
+            }
+        }
+
+        public override void OnCollisionEnter(Zombie_StateMachine_Manager stateMachineManager, Collision collision)
+        {
+
+        }
+
+        public override void OnTriggerEnter(Zombie_StateMachine_Manager stateMachineManager, Collider collider)
+        {
+
+        }
+    }
+}
+
+
---
+++ Game Dev 3 - Week 8/Assets/_Scripts/Zombie/StatePattern/Zombie_StateMachine_Moving.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using GameDevWithMarco.Enemy;
+
+namespace GameDevWithMarco.StatePattern
+{
+    public class Zombie_StateMachine_Moving : Zombie_StateMachine_BaseState
+    {
+        public override void EnterState(Zombie_StateMachine_Manager stateMachineManager)
+        {
+            //Plays a different animation based on the zombie type
+            switch (stateMachineManager.zombieParent.whatTypeIsThisZombie)
+            {
+                case Zombie_Parent.ZombieType.Walker:
+                    stateMachineManager.animScript.PlayWalkAnimation();
+                    break;
+                case Zombie_Parent.ZombieType.Runner:
+                    stateMachineManager.animScript.PlayRunAnimation();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public override void UpdateState(Zombie_StateMachine_Manager stateMachineManager)
+        {
+            if (stateMachineManager.zombieAiScript.isChasing == false)
+            {
+                stateMachineManager.SwitchState(stateMachineManager.idleState);
+            }
+        }
+
+        public override void OnCollisionEnter(Zombie_StateMachine_Manager stateMachineManager, Collision collision)
+        {
+
+        }
+
+        public override void OnTriggerEnter(Zombie_StateMachine_Manager stateMachineManager, Collider collider)
+        {
+
+        }
+    }
+}
